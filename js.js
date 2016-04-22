dd09f88b45feb443ba461f6d2d95188e96a40f01
tanbowen <tanbowensg@gmail.com>
2016-04-22 23:59:45 +0800

    filters

--- js.js
+++ js.js
@@ -3,11 +3,29 @@ new Vue({
 	el: '#deck-builder',
 	data: {
 		cards: _CARDS,
-		costs: [0,1,2,3,4,5,6,7,8,9]
+		costs: [0,1,2,3,4,5,6,7,8,9,''],
+		types: ['MINION', 'SPELL',''],
+		mechanics: [
+			{
+				displayName: '冲锋',
+				name:'CHARGE'
+			},
+			{
+				displayName: '亡语',
+				name: 'DEATHRATTLE'
+			},
+			{
+				displayName: '战吼',
+				name: 'BATTLECRY'
+			},
+		],
+		filters: {},
 	},
 	methods: {
-		filterCost: function(cost) {
-			this.cards = _.filter(_CARDS, {cost: cost})
+		filter: function({cost, type}) {
+			let params = {cost, type}
+			this.filters = _.merge(this.filters, params)
+			this.cards = _.filter(_CARDS, this.filters)
 		},
 	}
 })
