e6807879a12588be42259c5590b3a917d39d9b65
tanbowen <tanbowensg@gmail.com>
2016-04-22 22:58:31 +0800

    use vue

--- js.js
+++ js.js
@@ -1,3 +1,17 @@
+const _CARDS = _.take(_.filter(CARDS, 'collectible'),100)
+new Vue({
+	el: '#deck-builder',
+	data: {
+		cards: _CARDS,
+		costs: [0,1,2,3,4,5,6,7,8,9]
+	},
+	methods: {
+		filterCost: function(cost) {
+			this.cards = _.filter(_CARDS, {cost: cost})
+		},
+	}
+})
+
 function getIDByName(name) {
 	let temp
 	_.forEach(CARDS, val => {
